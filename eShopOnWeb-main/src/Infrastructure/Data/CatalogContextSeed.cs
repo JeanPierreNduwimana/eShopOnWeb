e4be76af0db51f3e8779f41e9fdc160ec219c9e1
Jean Pierre Nduwimana <1783527@cegepmontpetit.ca>
2024-11-22 10:43:12 -0500

    FCT: Changement de seed & Changements d'images

--- eShopOnWeb-main/src/Infrastructure/Data/CatalogContextSeed.cs
+++ eShopOnWeb-main/src/Infrastructure/Data/CatalogContextSeed.cs
@@ -61,11 +61,11 @@ public class CatalogContextSeed
     {
         return new List<CatalogBrand>
             {
-                new("Azure"),
-                new(".NET"),
-                new("Visual Studio"),
-                new("SQL Server"),
-                new("Other")
+                new("Choix du président"),
+                new("Bonne Maman"),
+                new("La Maison du Chocolat"),
+                new("Maison Blé"),
+                new("Maison Gosselin")
             };
     }
 
@@ -73,10 +73,11 @@ public class CatalogContextSeed
     {
         return new List<CatalogType>
             {
-                new("Mug"),
-                new("T-Shirt"),
-                new("Sheet"),
-                new("USB Memory Stick")
+                new("Pains"),
+                new("Viennoiseries"),
+                new("Pâtisseries"),
+                new("Sandwiches et en-cas"),
+                new("Gâteaux")
             };
     }
 
@@ -84,6 +85,33 @@ public class CatalogContextSeed
     {
         return new List<CatalogItem>
             {
+               
+                new(1,2, "Pain de campagne", "Pain de campagne", 2.5M, "http://catalogbaseurltobereplaced/images/products/21.png"),
+                new(1,1, "Baguette", "Baguette", 3.7M, "http://catalogbaseurltobereplaced/images/products/22.png"),
+                new(1,3, "Pain complet", "Pain complet", 5.5M, "http://catalogbaseurltobereplaced/images/products/23.png"),
+                new(1,5, "Pain de mie", "Pain de mie", 7.2M, "http://catalogbaseurltobereplaced/images/products/24.png"),
+
+                new(2,4, "Croissant", "Croissant", 2.5M, "http://catalogbaseurltobereplaced/images/products/25.png"),
+                new(2,5, "Pain au chocolat","Pain au chocolat", 2.5M, "http://catalogbaseurltobereplaced/images/products/26.png"),
+                new(2,4, "Chausson aux pommes","Chausson aux pommes", 2.5M, "http://catalogbaseurltobereplaced/images/products/27.png"),
+                new(2,3, "Pain au raisin","Pain au raisin", 2.5M, "http://catalogbaseurltobereplaced/images/products/28.png"),
+
+                new(3,2, "Tartelette","Tartelette", 2.5M, "http://catalogbaseurltobereplaced/images/products/29.png"),
+                new(3,1, "Éclair au chocolat","Éclair au chocolat", 2.5M, "http://catalogbaseurltobereplaced/images/products/30.png"),
+                new(3,5, "Mille-feuille","Mille-feuille", 2.5M, "http://catalogbaseurltobereplaced/images/products/31.png"),
+                new(3,4, "Macaron","Macaron", 2.5M, "http://catalogbaseurltobereplaced/images/products/32.png"),
+
+                new(4,3, "Quiche","Quiche", 2.5M, "http://catalogbaseurltobereplaced/images/products/13.png"),
+                new(4,2, "Croque-monsieur","Croque-monsieur", 2.5M, "http://catalogbaseurltobereplaced/images/products/14.png"),
+                new(4,1, "Pâté en croûte","Pâté en croûte", 2.5M, "http://catalogbaseurltobereplaced/images/products/15.png"),
+                new(4,5, "Salade composée","Salade composée", 2.5M, "http://catalogbaseurltobereplaced/images/products/16.png"),
+
+                new(5,4, "Gâteau au chocolat","Gâteau au chocolat", 2.5M, "http://catalogbaseurltobereplaced/images/products/17.png"),
+                new(5,3, "Gâteau de famille","Gâteau de famille", 2.5M, "http://catalogbaseurltobereplaced/images/products/18.png"),
+                new(5,2, "Tarte au citron","Tarte au citron", 2.5M, "http://catalogbaseurltobereplaced/images/products/19.png"),
+                new(5,1, "Tarte aux fruits","Tarte aux fruits", 2.5M, "http://catalogbaseurltobereplaced/images/products/20.png"),
+
+                /*
                 new(2,2, ".NET Bot Black Sweatshirt", ".NET Bot Black Sweatshirt", 19.5M,  "http://catalogbaseurltobereplaced/images/products/1.png"),
                 new(1,2, ".NET Black & White Mug", ".NET Black & White Mug", 8.50M, "http://catalogbaseurltobereplaced/images/products/2.png"),
                 new(2,5, "Prism White T-Shirt", "Prism White T-Shirt", 12,  "http://catalogbaseurltobereplaced/images/products/3.png"),
@@ -95,7 +123,7 @@ public class CatalogContextSeed
                 new(1,5, "Cup<T> White Mug", "Cup<T> White Mug", 12, "http://catalogbaseurltobereplaced/images/products/9.png"),
                 new(3,2, ".NET Foundation Sheet", ".NET Foundation Sheet", 12, "http://catalogbaseurltobereplaced/images/products/10.png"),
                 new(3,2, "Cup<T> Sheet", "Cup<T> Sheet", 8.5M, "http://catalogbaseurltobereplaced/images/products/11.png"),
-                new(2,5, "Prism White TShirt", "Prism White TShirt", 12, "http://catalogbaseurltobereplaced/images/products/12.png")
+                new(2,5, "Prism White TShirt", "Prism White TShirt", 12, "http://catalogbaseurltobereplaced/images/products/12.png") */
             };
     }
 }
--- eShopOnWeb-main/src/PublicApi/CatalogItemEndpoints/CatalogItemGetByIdEndpoint.cs
+++ eShopOnWeb-main/src/PublicApi/CatalogItemEndpoints/CatalogItemGetByIdEndpoint.cs
@@ -46,7 +46,7 @@ public class CatalogItemGetByIdEndpoint : IEndpoint<IResult, GetByIdCatalogItemR
             CatalogTypeId = item.CatalogTypeId,
             Description = item.Description,
             Name = item.Name,
-            PictureUri = _uriComposer.ComposePicUri(item.PictureUri),
+            PictureUri = item.PictureUri,
             Price = item.Price
         };
         return Results.Ok(response);
